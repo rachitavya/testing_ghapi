e4d66d1ee46f52a95a073c6c8f01819e17d24978
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-06-21 11:04:31 +0530

    Workflow dispatch with url variable

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
+import os
 import requests
 
-url="https://upcar.up.gov.in/en/page/advisory"
+url = os.getenv('REQUEST_URL', 'https://upcar.up.gov.in/en/page/advisory')
 try:
-    response=requests.get(url)
-    print("Success",response.status_code)
+    response = requests.get(url)
+    print("Success", response.status_code)
 except Exception as e:
     print("Failed", e)
-
