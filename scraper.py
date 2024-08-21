d1e013889a98b711914060bdad69d6c62b89022f
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-08-21 21:22:10 +0530

    pre header practice

--- scraper.py
+++ scraper.py
@@ -6,29 +6,22 @@ from requests.packages.urllib3.util.retry import Retry
 url = os.getenv('REQUEST_URL', 'https://upcar.up.gov.in/en/page/advisory')
 print(f"URL to request: {url}")
 
-session = requests.Session()
-retry = Retry(
-    total=3,
-    backoff_factor=1,
-    status_forcelist=[500, 502, 503, 504]
-)
-adapter = HTTPAdapter(max_retries=retry)
-session.mount('http://', adapter)
-session.mount('https://', adapter)
-
 try:
-    print("Sending request...")
-    response = session.get(url, timeout=20) 
-    print(f"Response received with status code: {response.status_code}")
-    if response.ok:
-        print("Request successful!")
-    else:
-        print("Request failed with status code:", response.status_code)
-except requests.exceptions.ConnectTimeout as e:
-    print("Connection timed out. Please check the server or your network connection.")
-except requests.exceptions.HTTPError as e:
-    print("HTTP error occurred:", e)
-except requests.exceptions.ReadTimeout as e:
-    print("Read timed out. The server did not send any data in the allotted amount of time.")
-except requests.exceptions.RequestException as e:
-    print("An error occurred during the request:", e)
+    print("here0")
+    response = requests.get(url)
+    print("here")
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    download_link = soup.find('a', class_='btn btn-success btn-xs external')['href']
+    uploading_date = soup.find('span', class_='pdf-details').find_all('strong')[-1].next_sibling.strip()
+    data={
+        "download_url":download_link,
+        "date":uploading_date
+    }
+
+    print(data)
+
+except Exception as e:
+    print(str(e))
+    print(data)
+
