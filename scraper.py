b7cffa57f4d2321fdf6cbee12ea3b7ca4f0ab4a0
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-08-22 08:26:11 +0530

    adding headers

--- scraper.py
+++ scraper.py
@@ -61,7 +61,10 @@ rename_districts = {
     'sonepur': 'subarnapur'
 }    
 try:
-    response = requests.get(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }    
+    response = requests.get(url, timeout=30)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     data = []
