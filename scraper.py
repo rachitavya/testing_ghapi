71225447f68b529dbd141e9da2553d657e553bee
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-08-22 08:35:28 +0530

    headers increadsed

--- scraper.py
+++ scraper.py
@@ -62,8 +62,12 @@ rename_districts = {
 }    
 try:
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-    }    
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Referer': 'https://ouat.ac.in/',
+    }
     response = requests.get(url, timeout=120)
     soup = BeautifulSoup(response.text, 'html.parser')
 
