f22b90b062c2aad1a374c6017567ae74453b5b40
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-08-22 08:28:51 +0530

    read timeout increased

--- scraper.py
+++ scraper.py
@@ -64,7 +64,7 @@ try:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
     }    
-    response = requests.get(url, timeout=30)
+    response = requests.get(url, timeout=120)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     data = []
