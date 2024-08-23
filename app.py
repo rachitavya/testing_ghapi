9279def8738f2ac0eb67f227f1cfb5871ea49ed3
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-08-23 10:03:26 +0530

    headers

--- app.py
+++ app.py
@@ -1,40 +1,57 @@
-from playwright.sync_api import sync_playwright
+import json
+import logging
+import requests
+from bs4 import BeautifulSoup
 
-def scraper_new():
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+
+def scraper():
+    url = "https://ouat.ac.in/quick-links/agro-advisory-services/"
+    rename_districts = {
+        "angul": "anugul",
+        "balasore": "baleshwar",
+        "boudh": "baudh",
+        "deogarh": "debagarh",
+        "keonjhar": "kendujhar",
+        "mayurbhanjha": "mayurbhanj",
+        "nabarangpur": "nabarangapur",
+        "sonepur": "subarnapur",
+    }
     try:
-        with sync_playwright() as p:
-            browser = p.firefox.launch(headless=True)
-            page = browser.new_page()
-            page.goto('https://ouat.ac.in/quick-links/agro-advisory-services/')
-            content = page.content()
-            # Assuming BeautifulSoup for HTML parsing
-            from bs4 import BeautifulSoup
-            soup = BeautifulSoup(content, 'html.parser')
+        # Perform the request and log headers
+        headers={'User-Agent': 'python-requests/2.31.0', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'}
+        response = requests.get(url, headers=headers)
 
-            data = []
-            districts = soup.find_all('div', class_='hide1')
-            for district in districts:
-                district_name = district.get('id')[:-1]
-                if district_name in rename_districts.keys():
-                    district_name = rename_districts[district_name]
-                data_dict = {'district_name': district_name}
-                table = district.find('table').find('tbody')
-                if len(table.select('tr')) > 0:
-                    rows = table.select('tr')[0]
-                else:
-                    continue
-                columns = rows.find_all('td')
-                date = columns[1].text.strip()
-                data_dict['date'] = date
-                english_link = columns[2].find('a')['href']
-                odia_link = columns[3].find('a')['href']
-                link_dict = {'english': english_link, 'odia': odia_link}
-                data_dict['link'] = link_dict
-                data.append(data_dict)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-            print(data)
+        data = []
+        districts = soup.find_all("div", class_="hide1")
+        for district in districts:
+            district_name = district.get("id")[:-1]
+            if district_name in rename_districts.keys():
+                district_name = rename_districts[district_name]
+            data_dict = {"district_name": district_name}
+            table = district.find("table").find("tbody")
+            if len(table.select("tr")) > 0:
+                rows = table.select("tr")[0]
+            else:
+                continue
+            columns = rows.find_all("td")
+            date = columns[1].text.strip()
+            data_dict["date"] = date
+            english_link = columns[2].find("a")["href"]
+            odia_link = columns[3].find("a")["href"]
+            link_dict = {"english": english_link, "odia": odia_link}
+            data_dict["link"] = link_dict
+            data.append(data_dict)
+
+        return {"statusCode": 200, "body": json.dumps(data)}
 
     except Exception as e:
-        print("Error", str(e))
+        logging.error(f"Error scraping website: {e}")
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
 
-scraper_new()
+
+print(scraper())
