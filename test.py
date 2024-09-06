ece9b6fc64a546b6d93f7ee2748b89f5c92c259e
Rachitavya Sharma <84073243+rachitavya@users.noreply.github.com>
2024-09-06 14:42:05 +0530

    reverse proxy

---
+++ test.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+url=os.environ.get("URL")
+try:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    download_link = soup.find('a', class_='btn btn-success btn-xs external')['href']
+    uploading_date = soup.find('span', class_='pdf-details').find_all('strong')[-1].next_sibling.strip()
+    data={
+        "download_url":download_link,
+        "date":uploading_date
+    }
+
+    return data
+
+except Exception as e:
+    print(str(e))
+    return {}
